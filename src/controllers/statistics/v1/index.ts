dc44b45edc19ee646b6f608a699a6c4ccd965a5f
TheUnconstrainedMind <141995559+amet-finance-core@users.noreply.github.com>
2023-08-31 17:33:43 +0400

    update

--- src/controllers/statistics/v1/index.ts
+++ src/controllers/statistics/v1/index.ts
@@ -1,12 +1,18 @@
 import {Request, Response} from "express";
 import connection from '../../../db/main'
+import {CONTRACT_TYPES, DEFAULT_CHAIN} from "../../../listener/constants";
 
 
 async function getStats(req: Request, res: Response) {
+    // const issued = await connection.db.collection(`Contract_${DEFAULT_CHAIN}`).countDocuments({type: {$ne: CONTRACT_TYPES.ZcbIssuer}});
+    // todo better to have one collection for general stats
+
     return res.json({
         data: {
-            totalPurchased: 241245.3,
-            totalRedeemed: 131245
+            issued: Math.round(Math.random() * 1000),
+            volumeUSD: Math.round(Math.random() * 1000000),
+            purchased: Math.round(Math.random() * 5000),
+            redeemed: Math.round(Math.random() * 3000)
         }
     })
 }
