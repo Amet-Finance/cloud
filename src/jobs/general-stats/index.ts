d3c9acba4b9c0116a31362c629314225136594a9
TheUnconstrainedMind <141995559+amet-finance-core@users.noreply.github.com>
2023-10-23 10:16:50 +0400

    updated the volume calculation

--- src/jobs/general-stats/index.ts
+++ src/jobs/general-stats/index.ts
@@ -1,12 +1,26 @@
 import connection from "../../db/main";
 import {CONTRACT_TYPES} from "../../listener/constants";
+import {CHAINS} from "../../modules/web3/constants";
+
+const Stables: { [key: string]: { [key: string]: { symbol: string, decimals: number } } } = {
+    [CHAINS.Mumbai]: {
+        "0x4714816Fa7cf2B52c789D243BEab4d58ca79e35B": {
+            symbol: "USDT",
+            decimals: 18
+        },
+        "0x06e1B6Ea003439210DBfF0555cBa86803a01913c": {
+            symbol: "USDT",
+            decimals: 18
+        }
+    }
+}
 
 async function updateGeneralStats() {
 
     try {
         const generalStats = {
             issued: 0,
-            volumeUSD: 420250,
+            volumeUSD: 0,
             purchased: 0,
             redeemed: 0
         }
@@ -14,6 +28,7 @@ async function updateGeneralStats() {
 
         for (const collection of collections) {
             if (collection.collectionName.startsWith("Contract_")) {
+                const chainId = collection.collectionName.replace("Contract_", "")
                 const chainStats = await collection.find().project({
                     purchased: 1,
                     redeemed: 1,
@@ -30,7 +45,15 @@ async function updateGeneralStats() {
                         generalStats.purchased += Number(contract.purchased)
                         generalStats.redeemed += Number(contract.redeemed)
 
-                        // todo add volume as well here
+                        const investmentTokenInfo = Stables?.[chainId]?.[contract.investmentToken]
+                        if (investmentTokenInfo && contract.purchased) {
+                            generalStats.volumeUSD += contract.purchased * (contract.investmentTokenAmount / 10 ** investmentTokenInfo.decimals)
+                        }
+
+                        const interestTokenInfo = Stables?.[chainId]?.[contract.interestToken]
+                        if (interestTokenInfo && contract.redeemed) {
+                            generalStats.volumeUSD += contract.redeemed * (contract.interestTokenAmount / 10 ** interestTokenInfo.decimals)
+                        }
                     }
                 }
             }
