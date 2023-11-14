ccb037487b1bdc73fd0e9847aec7622a3fbc292f
TheUnconstrainedMind <141995559+amet-finance-core@users.noreply.github.com>
2023-11-14 18:02:01 +0400

    Update index.ts

--- src/jobs/general-stats/index.ts
+++ src/jobs/general-stats/index.ts
@@ -2,20 +2,6 @@ import connection from "../../db/main";
 import {CONTRACT_TYPES} from "../../listener/constants";
 import {CHAINS} from "../../modules/web3/constants";
 
-// todo update this logic as well, maybe take from the Token collection with isStable or price updating regularly
-const Stables: { [key: string]: { [key: string]: { symbol: string, decimals: number } } } = {
-    [CHAINS.Mumbai]: {
-        "0x4714816Fa7cf2B52c789D243BEab4d58ca79e35B": {
-            symbol: "USDT",
-            decimals: 18
-        },
-        "0x06e1B6Ea003439210DBfF0555cBa86803a01913c": {
-            symbol: "USDT",
-            decimals: 18
-        }
-    }
-}
-
 async function updateGeneralStats() {
 
     try {
@@ -40,18 +26,31 @@ async function updateGeneralStats() {
                     interestTokenAmount: 1
                 }).toArray()
 
+
+                const tokensArray = await connection.db.collection(`Token_${chainId}`).find({
+                    isStable: true
+                }).toArray()
+
+                const stables = tokensArray.reduce((acc, item) => {
+                    acc[item._id.toString().toLowerCase()] = {
+                        decimals: item.decimals
+                    }
+                    return acc;
+                }, {} as any)
+
+
                 for (const contract of chainStats) {
                     if (contract.type !== CONTRACT_TYPES.ZcbIssuer) {
                         generalStats.issued += 1
                         generalStats.purchased += Number(contract.purchased)
                         generalStats.redeemed += Number(contract.redeemed)
 
-                        const investmentTokenInfo = Stables?.[chainId]?.[contract.investmentToken]
+                        const investmentTokenInfo = stables?.[contract.investmentToken.toLowerCase()]
                         if (investmentTokenInfo && contract.purchased) {
                             generalStats.volumeUSD += contract.purchased * (contract.investmentTokenAmount / 10 ** investmentTokenInfo.decimals)
                         }
 
-                        const interestTokenInfo = Stables?.[chainId]?.[contract.interestToken]
+                        const interestTokenInfo = stables?.[contract.interestToken.toLowerCase()]
                         if (interestTokenInfo && contract.redeemed) {
                             generalStats.volumeUSD += contract.redeemed * (contract.interestTokenAmount / 10 ** interestTokenInfo.decimals)
                         }
