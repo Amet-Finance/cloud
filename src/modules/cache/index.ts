034f0086eee7d7e741f27bdc5a26878d883c117b
TheUnconstrainedMind <141995559+amet-finance-core@users.noreply.github.com>
2023-11-18 18:50:56 +0400

    Update index.ts

--- src/modules/cache/index.ts
+++ src/modules/cache/index.ts
@@ -1,6 +1,8 @@
 import TokenService from '../token'
 
 async function InitiateCache(chainIds: number[]) {
+    await TokenService.cache(chainIds);
+
     setInterval(async () => {
         await TokenService.cache(chainIds)
     }, 60000)
