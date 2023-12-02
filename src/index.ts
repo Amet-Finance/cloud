8602416bda952841b2753176160d07c66e2e4c0b
TheUnconstrainedMind <126948085+TheUnconstrainedMind@users.noreply.github.com>
2023-12-02 19:19:32 +0400

    update

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,25 @@ import express, {Response} from 'express';
 import connection from './db/main';
 
 import ContractV1 from './routes/contract/v1'
+// import AffiliateV1 from './routes/affiliate/v1'
 import AddressV1 from './routes/address/v1'
 import StatisticsV1 from './routes/statistics/v1'
 import BalanceV1 from './routes/balance/v1'
 import TokenV1 from './routes/token/v1'
 import {CHAINS} from "./modules/web3/constants";
 import InitiateCache from "./modules/cache";
-
-
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
-// const limiter = rateLimit({
-//     windowMs: 60 * 1000, // 15 minutes
-//     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-//     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-// })
+const limiter = rateLimit({
+    windowMs: 5 * 1000, // 15 minutes
+    limit: 1000,
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
 
-// app.use(limiter)
+app.use(limiter)
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -30,6 +31,7 @@ app.use(express.urlencoded({extended: true}));
 app.use('/v1/token', TokenV1);
 app.use('/v1/address', AddressV1);
 app.use('/v1/contract', ContractV1);
+// app.use('/v1/affiliate', AffiliateV1);
 app.use('/v1/balance', BalanceV1)
 app.use('/v1/statistics', StatisticsV1);
 
