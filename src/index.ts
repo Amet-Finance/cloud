7f1c00a99ea88ce32556ed18b726cc101dd14c60
TheUnconstrainedMind <126948085+TheUnconstrainedMind@users.noreply.github.com>
2024-02-22 02:19:45 +0400

    up

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import "./config";
 import "./tracer";
 import "./handler";
 import cors from 'cors';
-import express, {Response} from 'express';
+import {Response} from 'express';
 import connection from './db/main';
 
 import app from "./server";
@@ -16,8 +16,6 @@ import SecurityMiddleware from "./routes/middlewares/v1";
 
 
 app.use(cors())
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
 
 app.use('/v1/token', TokenV1);
 app.use('/v1/address', AddressV1);
