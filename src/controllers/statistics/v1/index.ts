ee9eb5ebe4e542d3f2f21d0e00aa6a86844d9553
TheUnconstrainedMind <126948085+TheUnconstrainedMind@users.noreply.github.com>
2024-03-07 13:38:54 +0400

    Update index.ts

--- src/controllers/statistics/v1/index.ts
+++ src/controllers/statistics/v1/index.ts
@@ -1,12 +1,17 @@
 import {Request, Response} from "express";
 import connection from '../../../db/main'
+import ErrorV1 from "../../../routes/error/error";
 
 async function getStats(req: Request, res: Response) {
 
-    // todo this route has chainId, so the only chain data should be returned
-    // in general add general-stats-0x1335.... and etc..
-    const {chainId} = req.query
-    const data = await connection.db.collection("General").findOne({_id: 'general-stats' as any})
+    const {type} = req.query
+
+    const cleanType: any = type?.toString() || "";
+
+    const availableTypes = ['general-stats', 'tbv-daily-stats']
+    if (!availableTypes.includes(cleanType)) ErrorV1.throw("Invalid type");
+
+    const data = await connection.db.collection("General").findOne({_id: cleanType})
 
     return res.json({
         data
