bafa8b7449bc2cf869e2d1b57151bf6906d68782
TheUnconstrainedMind <126948085+TheUnconstrainedMind@users.noreply.github.com>
2024-04-18 00:03:02 +0400

    update

--- src/controllers/address/v1/index.ts
+++ src/controllers/address/v1/index.ts
@@ -1,80 +1,59 @@
-import {Request, Response} from "express";
-import connection from '../../../db/main'
-import {validateAddress} from "../../../modules/token/util";
+import { Request, Response } from 'express';
+import connection from '../../../db/main';
+import { validateAddress } from '../../../modules/token/util';
 
 async function get(req: Request, res: Response) {
-    try {
-        const {address} = req.query as any;
-        validateAddress(address);
-        const addressInfo = await connection.address.findOne({_id: address.toLowerCase()})
-        return res.json(addressInfo)
-    } catch (error: any) {
-        return res.status(400).json({
-            message: error.message
-        })
+    const { address } = req.query as any;
+    validateAddress(address);
+    const addressInfo = await connection.address.findOne({
+        _id: address.toLowerCase(),
+    });
+
+    if (!addressInfo) {
+        //todo update later
+        return res.json({ xp: 0 });
     }
+    return res.json(addressInfo);
 }
 
 async function post(req: Request, res: Response) {
+    const { address } = req.query as any;
+    const { twitter, telegram, reddit, image } = req.body;
 
-    try {
+    const addressInfo: any = {};
 
-        const {address} = req.query as any;
-        const {twitter, telegram, reddit, image} = req.body;
+    if (twitter) addressInfo.twitter = twitter;
 
+    if (telegram) addressInfo.telegram = telegram;
 
-        const addressInfo: any = {}
+    if (reddit) addressInfo.reddit = reddit;
 
-        if (twitter) {
-            addressInfo.twitter = twitter
-        }
+    if (image) addressInfo.image = image;
 
-        if (telegram) {
-            addressInfo.telegram = telegram
-        }
+    await connection.address.updateOne(
+        {
+            _id: address.toLowerCase(),
+        },
+        {
+            $set: addressInfo,
+        },
+        {
+            upsert: true,
+        },
+    );
 
-        if (reddit) {
-            addressInfo.reddit = reddit
-        }
-
-        if (image) {
-            addressInfo.image = image
-        }
-
-        await connection.address.updateOne({
-            _id: address.toLowerCase()
-        }, {
-            $set: addressInfo
-        }, {
-            upsert: true
-        })
-
-        return res.json({
-            success: true
-        })
-    } catch (error: any) {
-        return res.status(400).json({
-            success: false,
-            message: error.message
-        })
-    }
+    return res.json({
+        success: true,
+    });
 }
 
 async function del(req: Request, res: Response) {
-    try {
+    const { address } = req.query as any;
 
-        const {address} = req.query as any;
-
-        await connection.address.deleteOne({_id: address})
-        return res.json({
-            success: true
-        })
-    } catch (error: any) {
-        return res.status(400).json({
-            success: false,
-            message: error.message
-        })
-    }
+    await connection.address.deleteOne({ _id: address });
+    return res.json({
+        success: true,
+    });
 }
 
 export {
---
+++ src/jobs/address/xp.ts
@@ -0,0 +1,10 @@
+async function calculateXP() {
+
+    const xpList = {
+        Issuance: 10,
+        Purchase: 1,
+        Redeem: 2,
+
+    }
+
+}
