3452117031b77fc1304ad864bc2066487aa86c48
TheUnconstrainedMind <126948085+TheUnconstrainedMind@users.noreply.github.com>
2024-04-23 15:24:39 +0400

    Update v1.ts

--- src/controllers/validate/v1.ts
+++ src/controllers/validate/v1.ts
@@ -4,13 +4,16 @@ import axios from 'axios';
 async function twitter(req: Request, res: Response) {
     const { state, code } = req.query;
 
+    console.log(`state`, state);
+    console.log(`code`, code);
+
     const request = await axios.post(
         'https://api.twitter.com/2/oauth2/token',
         {
             code,
             grant_type: 'authorization_code',
             client_id: 'd295SDkyRFFPWV9mZDZMUV95RDg6MTpjaQ',
-            redirect_uri: 'https://www.example.com',
+            redirect_uri: 'https://api.amet.finance/validate/twitter',
             code_verifier: 'challenge',
         },
         {
