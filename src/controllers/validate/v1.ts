86ad3fe6682636d55b698276b71cc1bce762c796
TheUnconstrainedMind <126948085+TheUnconstrainedMind@users.noreply.github.com>
2024-04-23 15:47:46 +0400

    Update v1.ts

--- src/controllers/validate/v1.ts
+++ src/controllers/validate/v1.ts
@@ -22,22 +22,22 @@ async function twitter(req: Request, res: Response) {
             {
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded',
-                    'Authorization': `Basic AAAAAAAAAAAAAAAAAAAAAJnWtQEAAAAA4uWj4vELj1ziLpOAVv%2BV8TRq%2FfY%3Dy3HGNermDbHvGXJpMxTVksyptDDaDe3RawR4GIOwMxEZpwtBQx`,
+                    Authorization: `Basic AAAAAAAAAAAAAAAAAAAAAJnWtQEAAAAA4uWj4vELj1ziLpOAVv%2BV8TRq%2FfY%3Dy3HGNermDbHvGXJpMxTVksyptDDaDe3RawR4GIOwMxEZpwtBQx`,
                 },
             },
         );
 
         // {
-        //     Apr 23 11:36:48 ip-172-31-28-144 npm[248872]:   token_type: 'bearer',
-        //     Apr 23 11:36:48 ip-172-31-28-144 npm[248872]:   expires_in: 7200,
-        //     Apr 23 11:36:48 ip-172-31-28-144 npm[248872]:   access_token: 'enBhOUdhYkExNFk3a2M0azJfVktLaGtKYktqQ2NkenVoM1c1Q25CeXFDOGtsOjE3MTM4NzIyMDg0Mzk6MTowOmF0OjE',
-        //     Apr 23 11:36:48 ip-172-31-28-144 npm[248872]:   scope: 'follows.read offline.access users.read tweet.read follows.write',
-        //     Apr 23 11:36:48 ip-172-31-28-144 npm[248872]:   refresh_token: 'dDh5czU1NGZ0UW45SWJ1RTZoclJOa1c1Y2VJVGFESGhCMjRxT0xJM2dRWWpEOjE3MTM4NzIyMDg0Mzk6MTowOnJ0OjE'
-        //     Apr 23 11:36:48 ip-172-31-28-144 npm[248872]: }
+        //  token_type: 'bearer',
+        //    expires_in: 7200,
+        //    access_token: 'enBhOUdhYkExNFk3a2M0azJfVktLaGtKYktqQ2NkenVoM1c1Q25CeXFDOGtsOjE3MTM4NzIyMDg0Mzk6MTowOmF0OjE',
+        //    scope: 'follows.read offline.access users.read tweet.read follows.write',
+        //    refresh_token: 'dDh5czU1NGZ0UW45SWJ1RTZoclJOa1c1Y2VJVGFESGhCMjRxT0xJM2dRWWpEOjE3MTM4NzIyMDg0Mzk6MTowOnJ0OjE'
+        //  }
 
-        console.log(request.data);
+        // console.log(request.data);
 
-        return res.json(request.data)
+        return res.redirect("http://localhost:3000/auth/success")
     } catch (error: any) {
         console.log(error.message);
         console.log(error);
