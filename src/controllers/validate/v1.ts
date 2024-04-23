81cae94c542d543b875053b3163d4e48ed459281
TheUnconstrainedMind <126948085+TheUnconstrainedMind@users.noreply.github.com>
2024-04-23 15:29:05 +0400

    Update v1.ts

--- src/controllers/validate/v1.ts
+++ src/controllers/validate/v1.ts
@@ -8,28 +8,31 @@ async function twitter(req: Request, res: Response) {
     console.log(`state`, state);
     console.log(`code`, code);
 
-
     const data = qs.stringify({
         code,
         grant_type: 'authorization_code',
         client_id: 'd295SDkyRFFPWV9mZDZMUV95RDg6MTpjaQ',
         redirect_uri: 'https://api.amet.finance/validate/twitter',
         code_verifier: 'challenge',
-    })
+    });
 
-    const request = await axios.post(
-        'https://api.twitter.com/2/oauth2/token',data,
-        {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': `Basic `
+    try {
+        const request = await axios.post(
+            'https://api.twitter.com/2/oauth2/token',
+            data,
+            {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    Authorization: `Basic `,
+                },
             },
-        },
-    );
+        );
 
-    console.log(request.data);
-
-
+        console.log(request.data);
+    } catch (error: any) {
+        console.log(error.message);
+        console.log(error);
+    }
 }
 
 const ValidateControllerV1 = {
