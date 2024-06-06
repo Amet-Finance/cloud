2454271aee63e4c9aabc29e57c88594ecaedf3ad
TheUnconstrainedMind <126948085+TheUnconstrainedMind@users.noreply.github.com>
2024-06-06 23:45:08 +0400

    Update constants.ts

--- src/modules/api/constants.ts
+++ src/modules/api/constants.ts
@@ -10,10 +10,12 @@ const GRAPH_CONFIG: { [chainId: number]: { startBlock: number; url: string } } =
     [Chains.Base]: {
         startBlock: 14022551,
         url: 'https://subgraph.satsuma-prod.com/10c8c7e96744/unconstraineds-team--970943/Amet-Finance-8453/api',
+        //     https://gateway-arbitrum.network.thegraph.com/api/07d0ecc03fc3f9aeffea03e59ee0f10f/subgraphs/id/JBBiYxUHriMu5GpUtiuPLzroZbD6aGkaoipdLRn47BMy
     },
     [Chains.Arbitrum]: {
         startBlock: 218790445,
-        url: 'https://gateway-arbitrum.network.thegraph.com/api/07d0ecc03fc3f9aeffea03e59ee0f10f/subgraphs/id/4cT2qznsGi3npkRR6y2WuUPoEGMwcpUDewdCCJ6wHrvh',
+        url: 'https://subgraph.satsuma-prod.com/10c8c7e96744/unconstraineds-team--970943/Amet-Finance-42161/api',
+        // https://gateway-arbitrum.network.thegraph.com/api/07d0ecc03fc3f9aeffea03e59ee0f10f/subgraphs/id/4cT2qznsGi3npkRR6y2WuUPoEGMwcpUDewdCCJ6wHrvh
     },
 };
 
